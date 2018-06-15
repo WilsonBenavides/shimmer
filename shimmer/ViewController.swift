e5e4156b73634cdc3cda5f619a62594422eac184
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-06-15 12:47:21 -0700

    Initial Commit

---
+++ shimmer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shimmer
+//
+//  Created by willix on 15/06/18.
+//  Copyright © 2018 willix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
