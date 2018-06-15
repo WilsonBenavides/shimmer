abdee4b2fb0ca454fcfb6856cf122c5ad3d3ec21
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-06-15 13:09:05 -0700

    add gradient black and white

--- shimmer/ViewController.swift
+++ shimmer/ViewController.swift
@@ -15,14 +15,30 @@ class ViewController: UIViewController {
         
         view.backgroundColor = UIColor(white: 1, alpha: 0.1)
         
+        // darker text
         let darkTextLabel = UILabel()
         darkTextLabel.text = "Shimmer"
-        darkTextLabel.textColor = UIColor(white: 1, alpha: 1)
+        darkTextLabel.textColor = UIColor(white: 1, alpha: 0.2)
         darkTextLabel.font = UIFont.systemFont(ofSize: 80)
         darkTextLabel.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 400)
         darkTextLabel.textAlignment = .center
         
-        view.addSubview(darkTextLabel)
+        // darker text
+        let shinyTextLabel = UILabel()
+        shinyTextLabel.text = "Shimmer"
+        shinyTextLabel.textColor = .white
+        shinyTextLabel.font = UIFont.systemFont(ofSize: 80)
+        shinyTextLabel.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 400)
+        shinyTextLabel.textAlignment = .center
+        
+        view.addSubview(shinyTextLabel)
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.white.cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0, 1]
+        gradientLayer.frame = view.frame
+        
+        view.layer.addSublayer(gradientLayer)
     }
 
 }
